39a7ff20329d96263eb56d132f42a7d32eb9962e
Amanda <amanda@Amandas-MacBook-Pro-2.local>
2020-02-17 14:44:23 -0500

    xcode and firebase started

---
+++ bread/bread/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  bread
+//
+//  Created by Amanda on 2020-02-17.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
