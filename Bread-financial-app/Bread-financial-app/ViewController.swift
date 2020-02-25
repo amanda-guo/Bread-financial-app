200d5670a6e9cf28047af36502fad01876638f5a
Amanda <amanda@Amandas-MacBook-Pro-2.local>
2020-02-24 21:43:34 -0500

    new project started with single view

---
+++ Bread-financial-app/Bread-financial-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bread-financial-app
+//
+//  Created by Amanda on 2020-02-24.
+//  Copyright © 2020 Amanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
